8547654366617eec62448145525e58a5c95134cb
bimashazaman <bimashazaman1234@gmail.com>
2025-11-03 01:20:00 +0600

    initial

--- wp-content/themes/cnb-consulting-theme/inc/admin/page-creation-functions.php
+++ wp-content/themes/cnb-consulting-theme/inc/admin/page-creation-functions.php
@@ -15,17 +15,18 @@ if (!defined('ABSPATH')) {
  */
 function cnb_create_all_pages()
 {
+    $created_pages = get_option('cnb_pages_created', array());
 
-
-    // Only run once
-    if (get_option('cnb_pages_created')) {
-        return;
+    if ($created_pages === true || !is_array($created_pages)) {
+        $created_pages = array();
     }
 
     // Get all page definitions
     $core_pages = cnb_get_core_pages();
     $service_pages = cnb_get_service_pages();
     $pages = array_merge($core_pages, $service_pages);
+
+    $changes_made = false;
 
     foreach ($pages as $slug => $page_data) {
         // Check if page already exists
@@ -47,16 +48,30 @@ function cnb_create_all_pages()
                     update_post_meta($page_id, '_wp_page_template', $page_data['template']);
                 }
 
+                $created_pages[$slug] = (int) $page_id;
+                $changes_made = true;
                 error_log("Created page: {$page_data['title']} with ID: $page_id");
             } else {
                 error_log("Failed to create page: {$page_data['title']}");
             }
+        } else {
+            $created_pages[$slug] = (int) $existing_page->ID;
+
+            if (isset($page_data['template'])) {
+                $current_template = get_page_template_slug($existing_page->ID);
+
+                if ($current_template !== $page_data['template']) {
+                    update_post_meta($existing_page->ID, '_wp_page_template', $page_data['template']);
+                    $changes_made = true;
+                }
+            }
         }
     }
 
-    // Mark as created
-    update_option('cnb_pages_created', true);
+    update_option('cnb_pages_created', $created_pages);
 
-    // Flush rewrite rules to ensure permalinks work
-    flush_rewrite_rules();
+    // Flush rewrite rules only when changes were made
+    if ($changes_made) {
+        flush_rewrite_rules();
+    }
 }
